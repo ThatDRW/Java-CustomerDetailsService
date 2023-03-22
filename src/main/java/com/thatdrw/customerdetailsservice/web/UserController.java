4c1d0f25bd9c2cfa94d50cfa70790f582695e73d
ThatDRW <d.renswoude@gmail.com>
2023-03-22 14:18:06 +0100

    Feat: Added API Documentation.

--- src/main/java/com/thatdrw/customerdetailsservice/web/UserController.java
+++ src/main/java/com/thatdrw/customerdetailsservice/web/UserController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.thatdrw.customerdetailsservice.entity.User;
 import com.thatdrw.customerdetailsservice.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -24,11 +29,18 @@ public class UserController {
 
     UserService userService;
 
-	@GetMapping("/{id}")
+    @Operation(summary = "Find user by Id", description = "Provides user details matching the Id provided.")
+	@ApiResponse(responseCode = "200", description = "Succesful retrieval of user details.", content = @Content(array = @ArraySchema(schema = @Schema(implementation = User.class))))
+    @ApiResponse(responseCode = "403", description = "Operation requires Auth.")
+    @ApiResponse(responseCode = "404", description = "Customer not found.")
+    @GetMapping("/{id}")
 	public ResponseEntity<String> findById(@PathVariable Long id) {
 		return new ResponseEntity<>(userService.getUser(id).getUsername(), HttpStatus.OK);
 	}
 
+
+    @Operation(summary = "Register new user", description = "Register a new user.")
+    @ApiResponse(responseCode = "201", description = "Succesful creation on new user account.")
     @PostMapping("/register")
 	public ResponseEntity<HttpStatus> createUser(@Valid @RequestBody User user) {
 		userService.saveUser(user);
