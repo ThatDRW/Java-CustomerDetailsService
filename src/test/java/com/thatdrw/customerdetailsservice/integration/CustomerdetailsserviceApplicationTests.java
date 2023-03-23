1266b36592aa65c5a60589f478fc577fbdd674de
ThatDRW <d.renswoude@gmail.com>
2023-03-23 17:35:10 +0100

    Test: Dependency Injection Integration

--- src/test/java/com/thatdrw/customerdetailsservice/integration/CustomerdetailsserviceApplicationTests.java
+++ src/test/java/com/thatdrw/customerdetailsservice/integration/CustomerdetailsserviceApplicationTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +15,7 @@ import com.thatdrw.customerdetailsservice.web.CustomerController;
 import com.thatdrw.customerdetailsservice.web.UserController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class CustomerdetailsserviceApplicationTests {
 
