27be5c6883f8a38708e6688c5798439a3eaea13b
ThatDRW <d.renswoude@gmail.com>
2023-03-24 13:58:16 +0100

    Test: Added ApplicationExceptionHandler unit tests.

---
+++ src/test/java/com/thatdrw/customerdetailsservice/unit/ApplicationExceptionHandlerTest.java
@@ -0,0 +1,86 @@
+package com.thatdrw.customerdetailsservice.unit;
+
+import static org.junit.Assert.assertThrows;
+
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.MethodParameter;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import com.thatdrw.customerdetailsservice.ApplicationExceptionHandler;
+import com.thatdrw.customerdetailsservice.exception.ErrorResponse;
+import com.thatdrw.customerdetailsservice.web.CustomerController;
+import com.thatdrw.customerdetailsservice.web.UserController;
+
+// @SpringBootTest
+// @AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class ApplicationExceptionHandlerTest {
+
+    // @Autowired
+    // private UserController userController;
+
+    // @Autowired
+    // private CustomerController customerController;
+
+    // @Autowired
+    // private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // @Autowired
+    // private MockMvc mockMvc;
+
+    // @Test 
+    // public void contextLoads() {
+    //     assertNotNull(userController);        
+    //     assertNotNull(customerController);
+    //     assertNotNull(bCryptPasswordEncoder);
+    //     assertNotNull(mockMvc);        
+    // }
+
+    private ApplicationExceptionHandler handler = new ApplicationExceptionHandler();
+
+    @Test
+    public void handleResourceNotFoundExceptionTest() {
+        String message = "EntityNotFound";
+        ResponseEntity<Object> handled = handler.handleResourceNotFoundException(new EntityNotFoundException(message));
+        
+        assertEquals(Arrays.asList(message), ((ErrorResponse) handled.getBody()).getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, handled.getStatusCode());
+    }
+    
+    @Test
+    public void handleDataAccessExceptionTest() {
+        String message = "Cannot delete non-existing resource";
+        ResponseEntity<Object> handled = handler.handleDataAccessException(new EmptyResultDataAccessException(1));
+        
+        assertEquals(Arrays.asList(message), ((ErrorResponse) handled.getBody()).getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, handled.getStatusCode());
+    }
+
+    @Test
+    public void handleDataIntegrityViolationExceptionTest() {
+        String message = "Data Integrity Violation: we cannot process your request.";
+        ResponseEntity<Object> handled = handler.handleDataIntegrityViolationException(new DataIntegrityViolationException(""));
+
+        assertEquals(Arrays.asList(message), ((ErrorResponse) handled.getBody()).getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, handled.getStatusCode());
+    }
+}
