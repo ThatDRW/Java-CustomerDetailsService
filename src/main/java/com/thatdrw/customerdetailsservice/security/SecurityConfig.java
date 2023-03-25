e13b1a166573b3c5e9b3481377708dcd6b46a4d2
ThatDRW <d.renswoude@gmail.com>
2023-03-25 20:26:30 +0100

    Fix: Fixed SecurityFilterChain.

--- src/main/java/com/thatdrw/customerdetailsservice/security/SecurityConfig.java
+++ src/main/java/com/thatdrw/customerdetailsservice/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import com.thatdrw.customerdetailsservice.security.filter.AuthenticationFilter;
 import com.thatdrw.customerdetailsservice.security.filter.ExceptionHandlerFilter;
@@ -25,21 +26,37 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/authenticate");
-        http        
-            .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
-            .and()
-            .csrf().disable()
-            .authorizeHttpRequests()  
-            .requestMatchers("/v3/**").permitAll() // New Line: allows us to access the v3 API documentation without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /v3.
-            .requestMatchers("/swagger-ui/**").permitAll() // New Line: allows us to access the swagger-ui API documentation UI without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /swagger-ui.
-            .requestMatchers("/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
-            .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
-            .addFilter(authenticationFilter)
-            .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(antMatcher("/h2/**")).permitAll()
+                        .requestMatchers(antMatcher("/v3/**")).permitAll() // New Line: allows us to access the v3 API documentation without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /v3.
+                        .requestMatchers(antMatcher("/swagger-ui/**")).permitAll() // New Line: allows us to access the swagger-ui API documentation UI without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /swagger-ui.
+                        .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
+                        .anyRequest().authenticated()
+        )
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+                .addFilter(authenticationFilter)
+                .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.headers(headers -> headers.frameOptions().disable());
+        http.csrf().disable();
+
+
+        // http        
+        //     .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
+        //     .and()
+        //     .csrf().disable()
+        //     .authorizeHttpRequests()  
+        //     .requestMatchers(HttpMethod.GET, "/v3/**").permitAll() // New Line: allows us to access the v3 API documentation without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /v3.
+        //     .requestMatchers("/swagger-ui/**").permitAll() // New Line: allows us to access the swagger-ui API documentation UI without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /swagger-ui.
+        //     .requestMatchers(HttpMethod.GET, "/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+        //     .requestMatchers(HttpMethod.POST, "/h2/**").permitAll() // New Line: allows us to access the h2 console without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /h2.
+        //     .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
+        //     .anyRequest().authenticated()
+        //     .and()
+        //     .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+        //     .addFilter(authenticationFilter)
+        //     .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
+        //     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 }
