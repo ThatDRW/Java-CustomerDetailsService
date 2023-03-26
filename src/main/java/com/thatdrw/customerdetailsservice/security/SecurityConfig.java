27183f56d10badfabe43cc9323f744850bb1453b
ThatDRW <d.renswoude@gmail.com>
2023-03-26 16:00:53 +0200

    Fix: CORS Enabled and Configured for Angular.

--- src/main/java/com/thatdrw/customerdetailsservice/security/SecurityConfig.java
+++ src/main/java/com/thatdrw/customerdetailsservice/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import com.thatdrw.customerdetailsservice.security.filter.AuthenticationFilter;
@@ -26,13 +28,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
         authenticationFilter.setFilterProcessesUrl("/authenticate");
-        http.authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(antMatcher("/h2/**")).permitAll()
-                        .requestMatchers(antMatcher("/v3/**")).permitAll() // New Line: allows us to access the v3 API documentation without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /v3.
-                        .requestMatchers(antMatcher("/swagger-ui/**")).permitAll() // New Line: allows us to access the swagger-ui API documentation UI without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /swagger-ui.
-                        .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
-                        .anyRequest().authenticated()
-                     )
+        http.cors(withDefaults())
+                .authorizeHttpRequests((authz) -> authz
+                                .requestMatchers(antMatcher("/h2/**")).permitAll()
+                                .requestMatchers(antMatcher("/v3/**")).permitAll() // New Line: allows us to access the v3 API documentation without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /v3.
+                                .requestMatchers(antMatcher("/swagger-ui/**")).permitAll() // New Line: allows us to access the swagger-ui API documentation UI without the need to authenticate. ' ** '  instead of ' * ' because multiple path levels will follow /swagger-ui.
+                                .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
+                                .anyRequest().authenticated()
+                )
                 .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
                 .addFilter(authenticationFilter)
                 .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
@@ -40,7 +43,6 @@ public class SecurityConfig {
                 
         http.headers(headers -> headers.frameOptions().disable());
         http.csrf().disable();
-
 
         // http        
         //     .headers().frameOptions().disable() // New Line: the h2 console runs on a "frame". By default, Spring Security prevents rendering within an iframe. This line disables its prevention.
