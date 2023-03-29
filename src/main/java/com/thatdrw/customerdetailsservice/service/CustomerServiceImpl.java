0c2b454b922fee466d9fee2a3e0d7cb219a68b71
ThatDRW <d.renswoude@gmail.com>
2023-03-29 15:21:50 +0200

    Feat: @Transactional annotation added.

--- src/main/java/com/thatdrw/customerdetailsservice/service/CustomerServiceImpl.java
+++ src/main/java/com/thatdrw/customerdetailsservice/service/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thatdrw.customerdetailsservice.entity.Customer;
 import com.thatdrw.customerdetailsservice.exception.EntityNotFoundException;
@@ -20,28 +21,33 @@ public class CustomerServiceImpl implements CustomerService {
     private CustomerRepository customerRepository;
 
     @Override
+    @Transactional
     public Customer saveCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
 
     @Override
+    @Transactional
     public List<Customer> getCustomers() {
         return (List<Customer>)customerRepository.findAll();
     }
 
     @Override
+    @Transactional
     public Customer getCustomer(Long id) {
         Optional<Customer> customer = customerRepository.findById(id);
         return unwrapCustomer(customer, id);
     }
 
     @Override
+    @Transactional
     public List<Customer> findCustomer(String query) {
         Optional<List<Customer>> customer = customerRepository.findByFirstNameContainsOrLastNameContains(query, query);
         return unwrapCustomerList(customer, -1L);
     }
 
     @Override
+    @Transactional
     public Customer updateCustomerAddress(Long id, String address) {
         Customer customer = getCustomer(id);
         customer.setAddress(address);
--- src/main/java/com/thatdrw/customerdetailsservice/service/UserServiceImpl.java
+++ src/main/java/com/thatdrw/customerdetailsservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thatdrw.customerdetailsservice.entity.User;
 import com.thatdrw.customerdetailsservice.exception.EntityNotFoundException;
@@ -19,18 +20,21 @@ public class UserServiceImpl implements UserService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
+    @Transactional
     public User getUser(Long id) {
         Optional<User> user = userRepository.findById(id);
         return unwrapUser(user, id);
     }
 
     @Override
+    @Transactional
     public User getUser(String username) {
         Optional<User> user = userRepository.findByUsername(username);
         return unwrapUser(user, 404L);
     }
 
     @Override
+    @Transactional
     public User saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
