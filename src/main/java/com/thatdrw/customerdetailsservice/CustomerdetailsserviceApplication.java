d220183cf3951eec58006283fbe03b395b3ec8e6
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-02 01:47:14 +0200

    Fix: Code cleaned up.

--- src/main/java/com/thatdrw/customerdetailsservice/CustomerdetailsserviceApplication.java
+++ src/main/java/com/thatdrw/customerdetailsservice/CustomerdetailsserviceApplication.java
@@ -26,32 +26,21 @@ public class CustomerdetailsserviceApplication implements CommandLineRunner {
 		SpringApplication.run(CustomerdetailsserviceApplication.class, args);
 	}
 
+	private Address getTestAddress() {
+		return new Address("TestingStreet","123","1234AB","ThatVille");
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
-		Address testAddress1 = new Address("TestingStreet","123","1234AB","ThatVille");
-		Address testAddress2 = new Address("TestingStreet","123","1234AB","ThatVille");
-		Address testAddress3 = new Address("TestingStreet","123","1234AB","ThatVille");
-		Address testAddress4 = new Address("TestingStreet","123","1234AB","ThatVille");
-		Address testAddress5 = new Address("TestingStreet","123","1234AB","ThatVille");
-		Address testAddress6 = new Address("TestingStreet","123","1234AB","ThatVille");
-
 
 		Customer[] customers = new Customer[] {
-			new Customer("Josh", "Doe", new Date("Jan 21, 2000"), testAddress1),
-			new Customer("Jerry", "Doe", new Date("Feb 12, 1985"), testAddress2),
-			new Customer("Sonic", "Hedgehog, the", new Date("Aug 9, 1967"), testAddress3),
-			new Customer("Miles Tails", "Prower", new Date("Jun 14, 1985"), testAddress4),
-			new Customer("Amy", "Rose", new Date("Dec 30, 1999"), testAddress5),
-			new Customer("Shadow", "Hedgehog, the", new Date("May 21, 1006"), testAddress6),
+			new Customer("Josh", "Doe", new Date("Jan 21, 2000"), getTestAddress()),
+			new Customer("Jerry", "Doe", new Date("Feb 12, 1985"), getTestAddress()),
+			new Customer("Sonic", "Hedgehog, the", new Date("Aug 9, 1967"), getTestAddress()),
+			new Customer("Miles Tails", "Prower", new Date("Jun 14, 1985"), getTestAddress()),
+			new Customer("Amy", "Rose", new Date("Dec 30, 1999"), getTestAddress()),
+			new Customer("Shadow", "Hedgehog, the", new Date("May 21, 1006"), getTestAddress()),
 		};
-		// Customer[] customers = new Customer[] {
-		// 	new Customer("Josh", "Doe", new Date("Jan 21, 2000"), "12 AppleStreet, 15555 ThatVille"),
-		// 	new Customer("Jerry", "Doe", new Date("Feb 12, 1985"), "12 AppleStreet, 15555 ThatVille"),
-		// 	new Customer("Sonic", "Hedgehog, the", new Date("Aug 9, 1967"), "Sonic Drive 64, Christmas Island"),
-		// 	new Customer("Miles Tails", "Prower", new Date("Jun 14, 1985"), "66 Main Rd., West Side Island"),
-		// 	new Customer("Amy", "Rose", new Date("Dec 30, 1999"), "Unknown"),
-		// 	new Customer("Shadow", "Hedgehog, the", new Date("May 21, 1006"), "Space Colony ARK"),
-		// };
 
 		for (Customer c: customers) {
 			customerRepository.save(c);
