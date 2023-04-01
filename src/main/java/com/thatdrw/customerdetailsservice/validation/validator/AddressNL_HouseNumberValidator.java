514e1803024d4ed2fa82491ab1c97a6c4ed89c2e
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-01 22:41:07 +0200

    Fix: Better HouseNumber validation.

--- src/main/java/com/thatdrw/customerdetailsservice/validation/validator/AddressNL_HouseNumberValidator.java
+++ src/main/java/com/thatdrw/customerdetailsservice/validation/validator/AddressNL_HouseNumberValidator.java
@@ -9,13 +9,13 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class AddressNL_HouseNumberValidator implements ConstraintValidator <AddressNL_HouseNumber, String> {
 
-    Pattern onlyNumbersPattern = Pattern.compile("^[0-9]+$", Pattern.CASE_INSENSITIVE);
+    Pattern houseNumbersPattern = Pattern.compile("^[0-9]*[0-9a-zA-Z/-]+$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        boolean isOnlyNumbers = this.onlyNumbersPattern.matcher(value).find();
+        boolean isValid = this.houseNumbersPattern.matcher(value).find();
 
-        if (isOnlyNumbers)
+        if (isValid)
             return true;
             
         return false;
