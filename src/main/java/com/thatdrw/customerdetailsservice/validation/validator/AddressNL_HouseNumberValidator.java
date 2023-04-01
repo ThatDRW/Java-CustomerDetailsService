8715fb5d32e13fc253d4a3ff3f3ca80e34a20cbe
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-01 22:56:06 +0200

    Fix: Improved Regex pattern.

--- src/main/java/com/thatdrw/customerdetailsservice/validation/validator/AddressNL_HouseNumberValidator.java
+++ src/main/java/com/thatdrw/customerdetailsservice/validation/validator/AddressNL_HouseNumberValidator.java
@@ -9,7 +9,7 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class AddressNL_HouseNumberValidator implements ConstraintValidator <AddressNL_HouseNumber, String> {
 
-    Pattern houseNumbersPattern = Pattern.compile("^[0-9]*[0-9a-zA-Z/-]+$", Pattern.CASE_INSENSITIVE);
+    Pattern houseNumbersPattern = Pattern.compile("^[0-9]+[0-9a-zA-Z/.-]*$", Pattern.CASE_INSENSITIVE);
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
