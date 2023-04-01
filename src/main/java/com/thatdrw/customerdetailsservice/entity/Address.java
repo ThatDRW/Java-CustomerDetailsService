35748cd99c3f51e10fc94aa8ce0cf3526e5b9a0a
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-01 22:28:21 +0200

    Feat: Field validation added.

--- src/main/java/com/thatdrw/customerdetailsservice/entity/Address.java
+++ src/main/java/com/thatdrw/customerdetailsservice/entity/Address.java
@@ -1,5 +1,10 @@
 package com.thatdrw.customerdetailsservice.entity;
 
+import com.thatdrw.customerdetailsservice.validation.annotation.AddressNL_HouseNumber;
+import com.thatdrw.customerdetailsservice.validation.annotation.AddressNL_ZipCode;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,9 +14,20 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Address {
 
+    @NotBlank(message = "Street Name cannot be blank.")
+    @Size(min = 3, message = "Street Name needs to be longer than 3 characters.")
     private String streetName;
+
+    @AddressNL_HouseNumber(message = "House Number is not valid.")
+    @NotBlank(message = "House Number cannot be blank.")
     private String houseNumber;
+
+    @AddressNL_ZipCode(message = "Zip Code is not valid.")
+    @NotBlank(message = "Zip Code cannot be blank.")
     private String zipCode;
+
+    @NotBlank(message = "City cannot be blank.")
+    @Size(min = 2, message = "City needs to be longer than 2 characters.")
     private String city;
 
 }
