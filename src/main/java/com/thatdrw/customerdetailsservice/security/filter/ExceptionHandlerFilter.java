1c0a3cc56da96ae3350c2e1a6a9546288b1159fd
ThatDRW <112971591+ThatDRW@users.noreply.github.com>
2023-04-02 14:01:55 +0200

    Fix: Auth and Runtime errors properly wrapped.

--- src/main/java/com/thatdrw/customerdetailsservice/security/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/thatdrw/customerdetailsservice/security/filter/ExceptionHandlerFilter.java
@@ -1,6 +1,7 @@
 package com.thatdrw.customerdetailsservice.security.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,7 +10,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.thatdrw.customerdetailsservice.exception.EntityNotFoundException;
+import com.thatdrw.customerdetailsservice.exception.ErrorResponse;
 
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
@@ -19,16 +25,36 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } catch (EntityNotFoundException e) { //Feel free to create a separate function.
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            response.getWriter().write("Username doesn't exist");
+            response.getWriter().write(jsonErrorResponse("Username doesn't exist"));
             response.getWriter().flush();
         } catch (JWTVerificationException e) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getWriter().write("JWT NOT VALID");
+            response.getWriter().write(jsonErrorResponse("JWT NOT VALID"));
             response.getWriter().flush();
         } catch (RuntimeException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("BAD REQUEST");
+            response.getWriter().write(jsonErrorResponse("BAD REQUEST"));
             response.getWriter().flush();
         }  
+    }
+
+    private String jsonErrorResponse(String message) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList(message));
+
+        // Setup JavaTimeModule for Date parsing.
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        ObjectWriter writer = mapper.writer().withDefaultPrettyPrinter();
+        
+        try {
+            String json = writer.writeValueAsString(error);
+            return json;
+        } catch (JsonProcessingException e) {
+            System.err.println("JsonProcessingException caught trying to map an ErrorResponse with message: " + message);
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return "";
     }
 }
